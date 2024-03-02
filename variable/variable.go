3b0cf445727252960ab791e0cb0b68481b69be25
Dandi Irwanto <dandi.ir20@gmail.com>
2024-03-02 08:00:58 +0700

    learn: variable var

---
+++ variable/variable.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "Dandi Irwanto"
+	fmt.Println(name)
+
+	name = "Dand"
+	fmt.Println(name)
+
+	// cara kedua
+	// var nama sting = "Dandi I" optional
+	var nama = "Dandi I"
+	fmt.Println(nama)
+
+	nama = "Dandi Ir"
+	fmt.Println(nama)
+
+	// simplify without key var (note: declare := di awal dan untuk mengubah maka =)
+	namaewa := "Dand"
+	fmt.Println(namaewa)
+
+	namaewa = "Dan"
+	fmt.Println(namaewa)
+}
