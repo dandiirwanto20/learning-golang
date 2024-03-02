c2ea2ef5e858a5cad09f7c13097e926de052d95b
Dandi Irwanto <dandi.ir20@gmail.com>
2024-03-03 04:19:07 +0700

    learn: aritmatika opration mat

---
+++ matematika/mat.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+func main() {
+	// penjumlahan
+	a := 10
+	b := 10
+
+	c := a + b
+
+	fmt.Println(c)
+
+	// pengurangan
+	d := 20
+	e := 2
+
+	f := d - e
+
+	fmt.Println(f)
+
+	// perkalian
+	g := 10
+	h := 2
+
+	i := g * h
+
+	fmt.Println(i)
+
+	// pembagian
+	j := 2
+	k := 4
+
+	l := k / j
+
+	fmt.Println(l)
+
+	// modulo (hasil bagi)
+	m := 20
+	n := 3
+
+	o := m % n
+
+	fmt.Println(o)
+}
