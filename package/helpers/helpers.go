3737fb676f0506d7360e3beb8a6a9b179f80f4d6
Dandi Irwanto <dandi.ir20@gmail.com>
2024-03-07 10:18:32 +0700

    learn: package in go

---
+++ package/helpers/helpers.go
@@ -0,0 +1,5 @@
+package helpers
+
+func SayHello(name string) string {
+	return "Hello " + name
+}
---
+++ package/package.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"learning-golang/package/helpers"
+)
+
+func main() {
+	result := helpers.SayHello("Dandi")
+	fmt.Println(result)
+}
